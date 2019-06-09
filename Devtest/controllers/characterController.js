13e87183c3f174378cc10fc20a36c5c72afbdaa8
Neto6391 <neto6391@gmail.com>
2019-06-09 17:12:20 -0300

    Refactor filter in characterController

--- Devtest/controllers/characterController.js
+++ Devtest/controllers/characterController.js
@@ -1,30 +1,33 @@
-import fs from "fs";
-
-//Filter Data of Json Objects
-function filterCharacters(data) {
-	//Filter characters by for status 'Alive'
-	const filterData = data
-		.filter(element => {
-			return element.status != "Dead";
+/**
+ * Function to Filter JSON Data and return Data filtered
+ *
+ * This function Get Data from Characters Rick And Morty
+ * Get only characters 'Alive', and order By Quantity of
+ * episodes by form descent and name by form ascent, using
+ * function loacalCompare from JS
+ *
+ */
+function filteredCharacters(charactersData) {
+	const filterData = charactersData
+		.filter(character => {
+			return character.status === "Alive";
 		})
-		//Sort characters by order Ascendent
-		.sort((a, b) => {
-			return a.name.substr(0, 1) <= b.name.substr(0, 1) ? -1 : 1;
+		.sort((characterA, characterB) => {
+			return (
+				characterB.episode.length - characterA.episode.length ||
+				characterA.name.localeCompare(characterB.name)
+			);
 		});
-	//Sort Characters by Episode Ocorrence
-	filterData.sort((a, b) => {
-		return a.episode.length > b.episode.length ? -1 : 1;
-	});
-
 	return filterData;
 }
 
 //Get Characters and return to html
 export function getCharacters(req, res, next) {
-	const characterData = JSON.parse(
-		fs.readFileSync("data/characters.json", "utf-8")
-	);
-	const filterCharacterData = filterCharacters(characterData);
+	//Load Json from folder Data
+	const characterData = require("../data/characters.json");
+
+	//Filter Json Data from function
+	const filterCharacterData = filteredCharacters(characterData);
 
 	res.render("index", {
 		title: "Personagens",
